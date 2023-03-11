377c4d00fe039e61bb8fed14f76b0bddba238552
chetan gupta <chetangupta123raj@gmail.com>
2023-03-11 16:54:13 +0530

    initializes terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 provider "aws" {
   region= "us-east-1"
+  access_key = "sddflk"
+  secret_key = "slkdfs"
 }
 
 resource "aws_instance" "my_first_server" {
-  
+  instance_type = "t2.micro"
+  ami = "ami"
 }
