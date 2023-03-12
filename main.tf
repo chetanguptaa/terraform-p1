6325fa0e1a9143a6c674c5414af80b1d67e1d85d
chetan gupta <chetangupta123raj@gmail.com>
2023-03-12 18:51:12 +0530

    used terraform fmt

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.123.0.0/16"
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "dev"
@@ -9,10 +9,10 @@ resource "aws_vpc" "my_vpc" {
 }
 
 resource "aws_subnet" "my_public_subnet" {
-  vpc_id = aws_vps.my_vpc.id
-  cidr_block = "10.123.1.0/24"
+  vpc_id                  = aws_vps.my_vpc.id
+  cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1"
+  availability_zone       = "us-east-1"
   tags = {
     Name = "dev-public"
   }
