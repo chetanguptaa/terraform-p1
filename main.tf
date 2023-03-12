8d17fd4ecaf18bdffcbe682019b060ccc6d2a144
chetan gupta <chetangupta123raj@gmail.com>
2023-03-12 18:59:07 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,16 @@ resource "aws_internet_gateway" "my_internet_gateway" {
     Name = "dev-igw"
   }
 }
+
+resource "aws_route_table" "my_public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.my_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my_internet_gateway.id
+}
