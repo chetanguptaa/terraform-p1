76eed09e30801f1d5f69bbaf12132867c4be507e
chetan gupta <chetangupta123raj@gmail.com>
2023-03-13 18:26:46 +0530

    added datasources.tf

---
+++ datasources.tf
@@ -0,0 +1,8 @@
+data "aws_ami" "server_ami" {
+  most_recent = true
+  owners = ["0997201094727"]
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-10.04-amd64-server.*"]
+  }
+}
--- main.tf
+++ main.tf
@@ -60,3 +60,4 @@ resource "aws_security_group" "my_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
