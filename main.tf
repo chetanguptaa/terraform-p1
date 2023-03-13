63f8be6f6cad27c1601f988819b2265b827d973b
chetan gupta <chetangupta123raj@gmail.com>
2023-03-13 18:50:27 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -67,16 +67,17 @@ resource "aws_key_pair" "my_auth" {
 }
 
 resource "aws_instance" "dev-node" {
-  instance_type = "t2.micro"
-  ami           = data.aws_ami.server_ami.id
-  tags = {
-    Name = "dev-node"
-  }
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
   key_name               = aws_key_pair.my_auth.id
   vpc_security_group_ids = [aws_security_group.my_sg.id]
   subnet_id              = aws_subnet.my_public_subnet.id
+  user_data              = file("userdata.tpl")
   root_block_device {
     volume_size = 10
+  }
+  tags = {
+    Name = "dev-node"
   }
 }
 
